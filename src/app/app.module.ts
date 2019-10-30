dfc174f37ec4c60beebdf6eb6c276b669684abde
g-otn <g_otn@hotmail.com>
2019-10-30 08:35:23 -0300

    manual revert to 12187ca53ab98e4d984c978caa52eb0c18e460da

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,17 +10,8 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { HttpModule } from "@angular/http"
 import { FeedPageModule } from "../pages/feed/feed.module";
 import { IntroPageModule } from "../pages/intro/intro.module";
-import { MoovieProvider } from '../providers/moovie/moovie';
-import { FeedPage } from "../pages/feed/feed";
-import { ConfiguracoesPageModule } from '../pages/configuracoes/configuracoes.module';
-import { SobrePageModule } from '../pages/sobre/sobre.module';
-import { PerfilPageModule } from '../pages/perfil/perfil.module';
-import { FilmeDetalhesPageModule } from '../pages/filme-detalhes/filme-detalhes.module';
-import { CartolaProvider } from '../providers/cartola/cartola';
-import { AtletasPageModule } from '../pages/atletas/atletas.module';
 
 @NgModule({
   declarations: [
@@ -28,21 +19,13 @@ import { AtletasPageModule } from '../pages/atletas/atletas.module';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage,
-    FeedPage
+    TabsPage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
-    // Importando o módulo de feed
-    // FeedPageModule,
-    IntroPageModule,
-    HttpModule,
-    ConfiguracoesPageModule,
-    SobrePageModule,
-    PerfilPageModule,
-    FilmeDetalhesPageModule,
-    AtletasPageModule
+    FeedPageModule,
+    IntroPageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -50,15 +33,12 @@ import { AtletasPageModule } from '../pages/atletas/atletas.module';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage,
-    FeedPage
+    TabsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
-    CartolaProvider,
-    // MoovieProvider
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
 export class AppModule {}
