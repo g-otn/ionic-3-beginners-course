0bdca6c18d62dda31121c984faeee6339ee9224d
g-otn <g_otn@hotmail.com>
2019-11-04 12:28:38 -0300

    create config provider

---
+++ src/providers/config/config.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+
+const configKeyName = 'config'
+
+@Injectable()
+export class ConfigProvider {
+
+  private config = {
+    showSlide: false,
+    name: '',
+    username: ''
+  }
+
+  constructor() {
+  }
+
+  // Recupera os dados do LocalStorage
+  getConfigData(): object {
+    return JSON.parse(localStorage.getItem(configKeyName))
+  }
+
+  // Grava os dados do LocalStorage
+  setConfigData(showSlide?: boolean, name?: string, username?: string) {
+    const config = {
+      showSlide: false,
+      name: '',
+      username: ''
+    }
+
+    // Atualiza apenas o que foi mandado
+    if (showSlide) config.showSlide = showSlide
+    if (name) config.name = name
+    if (username) config.username = username
+
+    localStorage.setItem(configKeyName, JSON.stringify(config))
+
+  }
+
+}
